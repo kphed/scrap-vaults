d1697fa5da203b12280a266ef9b6a08531b45a84
kphed <kpcrypto69@gmail.com>
2023-02-09 08:28:47 -0500

    Update vault to support a single liquidityToken/liquidityPool

--- src/ScrapLyraVault.sol
+++ src/ScrapLyraVault.sol
@@ -15,43 +15,49 @@ import {ScrapLyraVaultShareERC1155} from "src/ScrapLyraVaultShareERC1155.sol";
 contract ScrapLyraVault is Errors, Owned, ReentrancyGuard {
     using SafeTransferLib for ERC20;
 
-    struct LiquidityToken {
-        ILiquidityPool pool;
-        ERC20 asset;
-        ScrapLyraVaultShare share;
-        ScrapLyraVaultShareERC1155 depositShare;
-        ScrapLyraVaultShareERC1155 withdrawShare;
-    }
+    ILiquidityToken public immutable liquidityToken;
+    ILiquidityPool public immutable liquidityPool;
+    ERC20 public immutable quoteAsset;
+    ScrapLyraVaultShare public immutable share;
+    ScrapLyraVaultShareERC1155 public immutable depositShare;
+    ScrapLyraVaultShareERC1155 public immutable withdrawShare;
 
-    mapping(ILiquidityToken => LiquidityToken) public liquidityTokens;
-
-    event SetLiquidityToken(
-        ILiquidityToken indexed liquidityToken,
-        ILiquidityPool indexed pool,
-        ERC20 indexed asset
-    );
     event Deposit(
-        ILiquidityToken indexed liquidityToken,
         address indexed msgSender,
         address indexed receiver,
-        uint256 queuedDepositId,
+        uint256 indexed queuedDepositId,
         uint256 amount,
         uint256 shareAmount
     );
 
-    error AlreadySet();
     error InvalidQueuedDeposit(
-        ILiquidityPool,
         uint256 queuedDepositId,
         uint256 amountLiquidity,
         uint256 depositInitiatedTime,
         ILiquidityPool.QueuedDeposit
     );
 
-    constructor() Owned(msg.sender) {}
+    constructor(ILiquidityToken _liquidityToken) Owned(msg.sender) {
+        if (address(_liquidityToken) == address(0)) revert Zero();
+
+        liquidityToken = _liquidityToken;
+        liquidityPool = ILiquidityPool(liquidityToken.liquidityPool());
+        quoteAsset = ERC20(liquidityPool.quoteAsset());
+        share = new ScrapLyraVaultShare("", "", 18, address(this));
+        depositShare = new ScrapLyraVaultShareERC1155(
+            msg.sender,
+            address(this)
+        );
+        withdrawShare = new ScrapLyraVaultShareERC1155(
+            msg.sender,
+            address(this)
+        );
+
+        // Set an allowance for the liquidity pool to transfer asset during deposits
+        quoteAsset.safeApprove(address(liquidityPool), type(uint256).max);
+    }
 
     function _verifyQueuedDeposit(
-        ILiquidityPool liquidityPool,
         uint256 queuedDepositId,
         uint256 amountLiquidity,
         uint256 depositInitiatedTime
@@ -66,7 +72,6 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard {
         ) return;
 
         revert InvalidQueuedDeposit(
-            liquidityPool,
             queuedDepositId,
             amountLiquidity,
             depositInitiatedTime,
@@ -75,59 +80,17 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard {
     }
 
     /**
-     * Set a Lyra liquidity token along with its associated pool and asset, and
-     * deploy its share contracts
-     *
-     * @param liquidityToken  ILiquidityToken  Liquidity token contract interface
-     */
-    function setLiquidityToken(
-        ILiquidityToken liquidityToken
-    ) external onlyOwner {
-        if (address(liquidityToken) == address(0)) revert Zero();
-
-        LiquidityToken storage token = liquidityTokens[liquidityToken];
-
-        if (address(token.pool) != address(0)) revert AlreadySet();
-
-        ILiquidityPool pool = ILiquidityPool(liquidityToken.liquidityPool());
-        ERC20 asset = ERC20(pool.quoteAsset());
-        token.pool = pool;
-        token.asset = asset;
-        token.share = new ScrapLyraVaultShare("", "", 18, address(this));
-        token.depositShare = new ScrapLyraVaultShareERC1155(
-            msg.sender,
-            address(this)
-        );
-        token.withdrawShare = new ScrapLyraVaultShareERC1155(
-            msg.sender,
-            address(this)
-        );
-
-        // Set an allowance for the liquidity pool to transfer asset during deposits
-        asset.safeApprove(address(pool), type(uint256).max);
-
-        emit SetLiquidityToken(liquidityToken, pool, asset);
-    }
-
-    /**
      * Deposit a Lyra liquidity pool quote asset for share tokens and earn rewards
      *
-     * @param liquidityToken  ILiquidityToken  Liquidity token contract interface
      * @param amount          uint256          Quote asset amount to deposit
      * @param receiver        address          Receiver of share tokens
      */
-    function deposit(
-        ILiquidityToken liquidityToken,
-        uint256 amount,
-        address receiver
-    ) external nonReentrant {
+    function deposit(uint256 amount, address receiver) external nonReentrant {
         if (address(liquidityToken) == address(0)) revert Zero();
         if (amount == 0) revert Zero();
 
-        LiquidityToken memory token = liquidityTokens[liquidityToken];
-
         // Reverts if liquidity token is not set
-        token.asset.safeTransferFrom(msg.sender, address(this), amount);
+        quoteAsset.safeTransferFrom(msg.sender, address(this), amount);
 
         // Enables us to determine the exact amount of liquidity tokens minted
         // in the event where there are zero live boards
@@ -137,7 +100,7 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard {
 
         // Signal a deposit to the liquidity pool, which may mint liquidity token
         // or queue the deposit, depending on the state of the protocol
-        token.pool.initiateDeposit(address(this), amount);
+        liquidityPool.initiateDeposit(address(this), amount);
 
         uint256 liquidityTokensMinted = liquidityToken.balanceOf(
             address(this)
@@ -145,34 +108,21 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard {
 
         if (liquidityTokensMinted == 0) {
             // Get the ID of our recently queued deposit
-            uint256 queuedDepositId = token.pool.nextQueuedDepositId() - 1;
+            uint256 queuedDepositId = liquidityPool.nextQueuedDepositId() - 1;
 
             // Verify that the queued deposit is actually ours (sanity check)
-            _verifyQueuedDeposit(
-                token.pool,
-                queuedDepositId,
-                amount,
-                block.timestamp
-            );
+            _verifyQueuedDeposit(queuedDepositId, amount, block.timestamp);
 
             // Mint deposit shares for the receiver, which accrues rewards but does
             // not allow the receiver to withdraw the underlying liquidity tokens
-            token.depositShare.mint(receiver, queuedDepositId, amount, "");
+            depositShare.mint(receiver, queuedDepositId, amount, "");
 
-            emit Deposit(
-                liquidityToken,
-                msg.sender,
-                receiver,
-                amount,
-                queuedDepositId,
-                amount
-            );
+            emit Deposit(msg.sender, receiver, amount, queuedDepositId, amount);
         } else {
             // Mint shares for the receiver if the liquidity was immediately added
-            token.share.mint(receiver, liquidityTokensMinted);
+            share.mint(receiver, liquidityTokensMinted);
 
             emit Deposit(
-                liquidityToken,
                 msg.sender,
                 receiver,
                 amount,
--- test/ScrapLyraVault.t.sol
+++ test/ScrapLyraVault.t.sol
@@ -22,13 +22,8 @@ contract ScrapLyraVaultTest is Errors, Test {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant VAULT_ROLE = keccak256("VAULT_ROLE");
 
-    ScrapLyraVault private immutable vault = new ScrapLyraVault();
-
-    event SetLiquidityToken(
-        ILiquidityToken indexed liquidityToken,
-        ILiquidityPool indexed pool,
-        ERC20 indexed asset
-    );
+    ScrapLyraVault private immutable vault =
+        new ScrapLyraVault(LYRA_USDC_LIQUIDITY_TOKEN);
 
     function _hasRole(
         AccessControl accessControl,
@@ -36,67 +31,5 @@ contract ScrapLyraVaultTest is Errors, Test {
         address account
     ) internal view returns (bool) {
         return accessControl.hasRole(role, account);
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                        setLiquidityToken TESTS
-    //////////////////////////////////////////////////////////////*/
-
-    function testCannotSetLiquidityTokenUnauthorized() external {
-        ILiquidityToken liquidityToken = LYRA_USDC_LIQUIDITY_TOKEN;
-
-        vm.prank(address(0));
-        vm.expectRevert(UNAUTHORIZED_ERROR);
-
-        vault.setLiquidityToken(liquidityToken);
-    }
-
-    function testCannotSetLiquidityTokenLiquidityTokenZero() external {
-        ILiquidityToken liquidityToken = ILiquidityToken(address(0));
-
-        vm.expectRevert(Zero.selector);
-
-        vault.setLiquidityToken(liquidityToken);
-    }
-
-    function testCannotSetLiquidityTokenAlreadySet() external {
-        ILiquidityToken liquidityToken = LYRA_USDC_LIQUIDITY_TOKEN;
-
-        vault.setLiquidityToken(liquidityToken);
-
-        vm.expectRevert(ScrapLyraVault.AlreadySet.selector);
-
-        vault.setLiquidityToken(liquidityToken);
-    }
-
-    function testSetLiquidityToken() external {
-        ILiquidityToken liquidityToken = LYRA_USDC_LIQUIDITY_TOKEN;
-        ILiquidityPool pool = ILiquidityPool(liquidityToken.liquidityPool());
-        ERC20 asset = ERC20(pool.quoteAsset());
-
-        vm.expectEmit(true, true, true, true, address(vault));
-
-        emit SetLiquidityToken(liquidityToken, pool, asset);
-
-        vault.setLiquidityToken(liquidityToken);
-
-        (
-            ILiquidityPool _pool,
-            ERC20 _asset,
-            ScrapLyraVaultShare share,
-            ScrapLyraVaultShareERC1155 depositShare,
-            ScrapLyraVaultShareERC1155 withdrawShare
-        ) = vault.liquidityTokens(liquidityToken);
-
-        assertEq(address(pool), address(_pool));
-        assertEq(address(asset), address(_asset));
-        assertTrue(address(share) != address(0));
-        assertTrue(_hasRole(share, VAULT_ROLE, address(vault)));
-        assertTrue(_hasRole(depositShare, ADMIN_ROLE, address(this)));
-        assertTrue(_hasRole(withdrawShare, ADMIN_ROLE, address(this)));
-        assertTrue(_hasRole(depositShare, VAULT_ROLE, address(vault)));
-        assertTrue(_hasRole(withdrawShare, VAULT_ROLE, address(vault)));
-        assertTrue(depositShare.supportsInterface(type(IERC1155).interfaceId));
-        assertTrue(withdrawShare.supportsInterface(type(IERC1155).interfaceId));
     }
 }
