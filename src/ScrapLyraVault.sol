7d34d467a5630d64dc1735392f8fc9a9a1b1a43e
kphed <kpcrypto69@gmail.com>
2023-02-10 07:29:42 -0500

    Remove unused imports, add missing param validation, and clean up comments

--- src/ScrapLyraVault.sol
+++ src/ScrapLyraVault.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import {Owned} from "solmate/auth/Owned.sol";
-import {ERC20} from "solmate/tokens/ERC20.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 import {ReentrancyGuard} from "solmate/utils/ReentrancyGuard.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
@@ -12,10 +11,9 @@ import {ILiquidityToken} from "src/interfaces/ILiquidityToken.sol";
 import {ILiquidityPool} from "src/interfaces/ILiquidityPool.sol";
 import {Errors} from "src/utils/Errors.sol";
 import {ScrapLyraVaultShareERC1155} from "src/ScrapLyraVaultShareERC1155.sol";
-import {IFlywheelRewards} from "src/interfaces/IFlywheelRewards.sol";
 import {IMultiDistributor} from "src/interfaces/IMultiDistributor.sol";
 
-contract ScrapLyraVault is Errors, Owned, ReentrancyGuard, ERC20 {
+contract ScrapLyraVault is Errors, ReentrancyGuard, ERC20 {
     using SafeTransferLib for ERC20;
     using SafeCastLib for uint256;
     using FixedPointMathLib for uint256;
@@ -74,7 +72,7 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard, ERC20 {
         string memory _name,
         string memory _symbol,
         uint8 _decimals
-    ) Owned(msg.sender) ERC20(_name, _symbol, _decimals) {
+    ) ERC20(_name, _symbol, _decimals) {
         if (address(_liquidityToken) == address(0)) revert Zero();
 
         liquidityToken = _liquidityToken;
@@ -116,7 +114,7 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard, ERC20 {
     }
 
     function _getAccruedRewards() private returns (uint256) {
-        // TODO: Improve efficiency of this method once its deployed on Arbitrum
+        // TODO: Improve efficiency once the Lyra contract is deployed on Arbitrum
 
         uint256 balanceBeforeClaim = STK_LYRA.balanceOf(address(this));
         IERC20[] memory claimTokens = new IERC20[](1);
@@ -194,14 +192,20 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard, ERC20 {
         return (_accrueUser(user, state), _accrueUser(secondUser, state));
     }
 
-    function _accrueMint(address _to, uint256 _amount) private {
-        _accrue(_to);
-        _mint(_to, _amount);
+    function _accrueMint(address to, uint256 amount) private {
+        if (to == address(0)) revert Zero();
+        if (amount == 0) revert Zero();
+
+        _accrue(to);
+        _mint(to, amount);
     }
 
-    function _accrueBurn(address _from, uint256 _amount) private {
-        _accrue(_from);
-        _burn(_from, _amount);
+    function _accrueBurn(address from, uint256 amount) private {
+        if (from == address(0)) revert Zero();
+        if (amount == 0) revert Zero();
+
+        _accrue(from);
+        _burn(from, amount);
     }
 
     function transfer(
@@ -271,7 +275,7 @@ contract ScrapLyraVault is Errors, Owned, ReentrancyGuard, ERC20 {
     }
 
     /**
-     * Deposit a Lyra liquidity pool quote asset for share tokens and earn rewards
+     * Deposit a Lyra liquidity pool quote asset for share tokens
      *
      * @param receiver  address  Receiver of deposit or vault share tokens
      * @param amount    uint256  Quote asset amount to deposit
